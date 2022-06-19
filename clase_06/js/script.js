ad855279f65a0993b5e5ae07c3b557abacc1861d
Avixys <nico_barbano@hotmail.com>
2022-06-19 19:13:38 -0300

    Agregado clases, metodos y arrays para entrega

--- clase_06/js/script.js
+++ clase_06/js/script.js
@@ -1,32 +1,108 @@
 class Product {
-    constructor (name, price, quantity) {
+    constructor (id, name, price, quantity) {
+        this.id = id;
         this.name = name;
         this.price = parseFloat(price);
         this.quantity = quantity;
-        //this.stock = true;
     }
 
     calculateTax () {
         return this.price * 1.21;
     }
-
 }
 
-const products = [];
+class ProductStock{
+    constructor(){
+        this.products=[]
+    }
+
+    addProduct(prod){
+        this.products.push(prod)
+    }
+
+    getProducts(){
+        return this.products;
+    }
+
+    getProduct(id){
+        return this.products.find(p=>p.id===id)
+    }
+
+    getProductsWithStock(){
+        return this.products.filter(p=>p.quantity>0)
+    }
+}
+
+class Cart {
+    constructor (user) {
+        this.user = user;
+        this.products = [];
+    }
+
+    addProduct(prod){
+        let allreadyIn = this.products.find(p=>p.id===prod.id);
+        if (allreadyIn !==undefined){
+            allreadyIn.q += prod.q;
+            let remainingProducts = this.products.filter(p=>p.id !== prod.id);
+            this.products = [...remainingProducts, allreadyIn];
+        } else {
+        this.products.push(prod);
+        }
+    }
+}
+
+const products = new ProductStock();
+let productId="";
 do {
-    productName = prompt ("Ingrese nombre de prodcuto o ESC para finalizar");
-    if (productName === "ESC" || productName === "Esc" || productName === "esc") {
+    productId = prompt ("Ingrese ID de prodcuto o ESC para finalizar");
+    if (productId === "ESC" || productId === "Esc" || productId === "esc") {
         break;
     } else {
+        let productName = prompt("ingrese nombre de producto");
         let productPrice = prompt ("Ingrese precio del prodcuto");
         let prodcutQuantity = prompt ("Ingrese cantidad del producto");
-        products.push (new Product (productName, productPrice, prodcutQuantity));
+        products.addProduct (new Product (productId, productName, productPrice, prodcutQuantity));
     }
-} while (productName != "ESC" || productName != "Esc" || productName != "esc")
+} while (productId.toLowerCase().trim() !== "esc")
 
-for (const product of products) {
+for (const product of products.getProducts()) {
+    console.log("ID: " + productId);
     console.log("Nombre: " + product.name);
     console.log("Precio: $" + product.price);
     console.log("Cantidad disponible: " + product.quantity);
     console.log("El precio con IVA es : $" + product.calculateTax());
 }
+
+let user = prompt("Ingrese su usuario");
+
+//agregar validación de usuario
+let cart = new Cart (user);
+
+let prodsList = "Elija un Id de producto o esc para finalizar:\n"
+for(const p of products.getProducts()){
+    prodsList += `${p.id}) ${p.name}\n`;
+}
+
+let cartProductId="";
+do {
+    cartProductId = prompt(prodsList);
+    let cartProdSel = products.getProduct(cartProductId);
+    if(cartProdSel!==undefined){
+        let cartProductQ = prompt(`ingrese la cantidad de productos (máximo ${cartProdSel.quantity})`);
+        if(parseInt(cartProductQ)>0 && parseInt(cartProductQ)<=cartProdSel.quantity){
+            cart.addProduct({id:cartProductId, q:cartProductQ});
+            alert("Producto agregado al carro");
+        }
+    }
+}while(cartProductId.toLowerCase().trim()!=="esc")
+
+let prodsCart = "Los porudctos en su carrito son:\n"
+
+let total = 0;
+for (const p of cart.products) {
+    let cartProduct = products.getProduct(p.id); 
+    prodsCart += `${cartProduct.name}, Cantidad: ${p.q}, Precio unitario: $${cartProduct.price}, subtotal: $${parseFloat(p.q*cartProduct.price).toFixed(2)}\n`;
+    total += parseFloat(p.q*cartProduct.price);
+}
+prodsCart +=`\nTotal: $${parseFloat(total).toFixed(2)}`;
+alert(prodsCart);
